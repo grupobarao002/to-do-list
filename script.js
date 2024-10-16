f6621b924d1b7ae358cbdede940bb3820a3be6a8
grupobarao002 <faduapassos264@gmail.com>
2024-10-16 10:18:14 -0300

    009

--- script.js
+++ script.js
@@ -3,8 +3,30 @@ const btnAddtarefa = document.getElementById('btAddTarefa');
 const listaTarefa = document.getElementById('listaTarefa');
 
 btnAddtarefa.addEventListener('click',function(){
+    const tarefa = novaTarefa.value;
+     
 
-alert(novaTarefa);
+    if(tarefa !=" "){
+
+       const li = document.createElement('li');
+
+       li.textContent = novaTarefa;
+
+       const botaoRemover = document.createElement('button');
+       bataoRemover.textContent = "remover";
+
+       botaoRemover.addEventListener('click', function(){
+       li.remove();
+    )};
+
+
+        li.appendChid(botaoRemover);
+        listaTarefa.appendChild(li);
+    
+        novaTarefa.value = "";
+
+        
+    }
 
 
 
